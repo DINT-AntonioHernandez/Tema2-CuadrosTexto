e8c18a18b6b021adabf9c13ca8eab0b8e4397ac2
puercoespin <hs3designer@gmail.com>
2023-10-14 16:53:47 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CuadrosTexto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void help_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                nombrehelpTextBlock.Visibility = Visibility.Visible;
+                apellidohelpTextBlock.Visibility = Visibility.Visible; 
+            }
+            else if(e.Key == Key.F2)
+            {
+                try {
+                    int numero = int.Parse(edadentradaTextBox.Text);
+                }
+                catch {
+                    edadhelpTextBlock.Visibility = Visibility.Visible;
+                }
+            }
+        }
+    }
+}
